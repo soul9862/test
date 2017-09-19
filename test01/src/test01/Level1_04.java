ef7ee9c09e8d71b403e21aa3cc17c0f48c135a79
soul9862-sub <soul9862@gmail.com>
2017-09-19 18:21:57 +0900

    약수의 합

---
+++ test01/src/test01/Level1_04.java
@@ -0,0 +1,25 @@
+package test01;
+
+class sumDivisor{
+	public int sumDivisor(int num) {
+		int answer = 0;
+		
+		for(int i = 1; i <= num; i++) {
+			if(num % i == 0) {
+				answer += i;
+			}
+		}
+		
+		return answer;
+	}
+}
+
+public class Level1_04 {
+	public static void main(String[] args) {
+		sumDivisor c = new sumDivisor();
+		
+		int num = 5;
+		
+		System.out.println(c.sumDivisor(num));
+	}
+}
