968f6961452bef8c0cd9b7fec4eb6272bae68ab2
soul9862-sub <soul9862@gmail.com>
2017-09-19 18:21:13 +0900

    행렬의 덧셈

--- test01/src/test01/Level1_02.java
+++ test01/src/test01/Level1_02.java
@@ -1,5 +1,37 @@
 package test01;
 
-public class Level1_02 {
+class SumMatrix {
+	int[][] sumMatrix(int n, int m, int[][] A, int[][] B) {
+		int[][] answer = new int [n][m];
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				answer[i][j] = A[i][j] + B[i][j];
+			}
+		}
+		
+		
 
+		return answer;
+	}
 }
+
+public class Level1_02 {
+	public static void main(String[] args) {
+		
+		SumMatrix c = new SumMatrix();
+		int n = 2;
+		int m = 2;
+		int[][] A = { { 1, 2 }, { 2, 3 } };
+		int[][] B = { { 3, 4 }, { 5, 6 } };
+		int[][] answer = c.sumMatrix(n, m,A, B);
+		if (answer[0][0] == 4 && answer[0][1] == 6 && 
+				answer[1][0] == 7 && answer[1][1] == 9) {
+			System.out.println("맞았습니다. 제출을 눌러 보세요");
+		} else {
+			System.out.println("틀렸습니다. 수정하는게 좋겠어요");
+		}
+
+	}
+}
+
