1da844f275af687f45d5550d6ea648ef69fc4174
soul9862-sub <soul9862@gmail.com>
2017-09-26 19:03:10 +0900

    삼각형 출력하기

---
+++ test01/src/test01/Level1_08.java
@@ -0,0 +1,23 @@
+package test01;
+
+public class Level1_08 {
+	public String printTriangle(int num){
+		String result = "";
+		String star = "*";
+		
+		for(int i = 0; i < num; i++) {
+			result += star + "\n";
+			star += "*";
+		}
+	
+		return result;		
+	}
+	
+	public static void main(String[] args) {
+
+		Level1_08 pt = new Level1_08();
+		System.out.println( pt.printTriangle(3) );
+		
+	}
+}
+
