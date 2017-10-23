b60484d5dcbd5b10e82260f40de1cbc9fef74151
park <soul9862@naver.com>
2017-10-23 14:21:21 +0900

    최솟값 만들기

---
+++ test01/src/Level2/Level2_02.java
@@ -0,0 +1,29 @@
+package Level2;
+
+public class Level2_02 {
+    public int getMinSum(int []A, int []B)
+    {
+        int answer = 0;
+
+        int temp;
+        
+        for(int i = 0; i < B.length/2; i++) {
+        	temp = B[i];
+        	B[i] = B[B.length-1-i];
+        	B[B.length-1-i] = temp;
+        }
+        
+        for(int i = 0; i < A.length; i++) {
+        	answer += A[i] * B[i];
+        }
+        
+        return answer;
+    }
+    public static void main(String[] args)
+    {
+        Level2_02 test = new Level2_02();
+        int []A = {1,2};
+        int []B = {3,4};
+        System.out.println(test.getMinSum(A,B));
+    }
+}
