e713c5d10572c23d557ef64a743208dfe3cf7382
Dextet0526 <soul9862@naver.com>
2019-06-27 12:22:02 +0900

    하샤드 수 수정(형변환 추가)

--- test01/src/Level2/Level2_01.java
+++ test01/src/Level2/Level2_01.java
@@ -4,11 +4,17 @@ public class Level2_01 {
 	public boolean isHarshad(int num){
 
 		String test = "";
-		test = String.valueOf(num);
+		//test = String.valueOf(num);
+		//s_num = Integer.toString(i_num);
+		test = Integer.toString(num);
+		System.out.println("test 값 : " + test);
+		System.out.println("test 문자 길이 : " + test.length());
+		
 		int temp = 0;
 		
 		for(int i = 0; i < test.length(); i++) {
 			temp += Integer.parseInt(test.substring(i, i+1));
+			System.out.println(test.substring(i, i+1));
 		}
 		if(num % temp == 0) {
 			return true;
@@ -21,6 +27,6 @@ public class Level2_01 {
        // 아래는 테스트로 출력해 보기 위한 코드입니다.
 	public static void  main(String[] args){
 		Level2_01 sn = new Level2_01();
-		System.out.println(sn.isHarshad(11));
+		System.out.println(sn.isHarshad(13));
 	}
 }
