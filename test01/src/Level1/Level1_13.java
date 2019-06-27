74e3c37a58fb3fa4efa14da73a7d0e6fd0b3ba5f
Dextet0526 <soul9862@naver.com>
2019-06-27 14:41:42 +0900

    프로그래머스 Level1 예산

---
+++ test01/src/Level1/Level1_13.java
@@ -0,0 +1,29 @@
+package Level1;
+
+import java.util.Arrays;
+
+public class Level1_13 {
+	
+	  public int solution(int[] d, int budget) {
+	      int answer = 0;
+	      
+	      Arrays.sort(d);
+	      for(int i = 0; i< d.length; i++) {
+	    	  budget -= d[i];
+	    	  if(budget >= 0){
+	    		  answer += 1;
+	    	  }else {
+	    		  break;
+	    	  }
+	      }
+	      return answer;
+	  }
+	
+	public static void main(String[] args) {
+		Level1_13 test = new Level1_13();
+		int[] d = new int[]{100,2,2,1,1,5};
+		int budget = 300;
+		System.out.println(test.solution(d, budget));
+	} 
+	
+}
