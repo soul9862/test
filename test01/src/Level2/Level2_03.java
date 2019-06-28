be77952f6fb9efe7b955a9e34cce5f28cf5b4fb9
Dextet0526 <soul9862@naver.com>
2019-06-28 14:51:49 +0900

    코라츠 추측(주석 추가 및 테스팅)

--- test01/src/Level2/Level2_03.java
+++ test01/src/Level2/Level2_03.java
@@ -3,20 +3,21 @@ package Level2;
 public class Level2_03 {
 	public int collatz(int num) {
 		int answer = 0;
+		long temp = num;
 
-		while(num > 1) {
+		while(true) {
 			
-			if(num % 2 == 0) {
-				num = num / 2;
+			if(temp % 2 == 0) {
+				temp = temp / 2;
 			}else {
-				num = num * 3 + 1;
+				temp = temp * 3 + 1;
 			}
 			answer++;
 			
 			if(answer == 500) {
 				answer = -1;
 				break;
-			}
+			}else if(temp == 1) break;
 		}
 		
 		return answer;
@@ -25,7 +26,7 @@ public class Level2_03 {
 	// 아래는 테스트로 출력해 보기 위한 코드입니다.
 	public static void main(String[] args) {
 		Level2_03 c = new Level2_03();
-		int ex = 125266;
+		int ex = 626331;
 		System.out.println(c.collatz(ex));
 	}
 }
