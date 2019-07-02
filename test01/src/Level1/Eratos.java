ba1ac176e485d28c0a1b4a971da715906fb66832
Dextet0526 <soul9862@naver.com>
2019-07-02 15:13:44 +0900

    에라토스테네스의 체(소수  찾기), 참조 : 위키피디아

---
+++ test01/src/Level1/Eratos.java
@@ -0,0 +1,48 @@
+package Level1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// 에라토스테네스의 체 (소수 찾기)
+public class Eratos {
+	public static void main(String[] args) {
+		// ArrayList로 구현
+		ArrayList<Boolean> primeList;
+
+		// 사용자로부터의 콘솔 입력
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+
+		// n <= 1 일 때 종료
+		if(n <= 1) return;
+
+		// n+1만큼 할당
+		primeList = new ArrayList<Boolean>(n+1);
+		// 0번째와 1번째를 소수 아님으로 처리
+		primeList.add(false);
+		primeList.add(false);
+		// 2~ n 까지 소수로 설정
+		for(int i=2; i<=n; i++ )
+			primeList.add(i, true);
+
+		// 2 부터  ~ i*i <= n
+		// 각각의 배수들을 지워간다.
+		for(int i=2; (i*i)<=n; i++){
+			if(primeList.get(i)){
+				for(int j = i*2; j<=n; j+=i) primeList.set(j, false);
+				//j의 시작값을 i*i로 하셨는데 i*2가 맞는거 같아요
+			}
+		}
+		StringBuffer sb = new StringBuffer();
+		sb.append("{");
+		for(int i=0; i<=n; i++){
+			if(primeList.get(i) == true){
+				sb.append(i);
+				sb.append(",");
+			}
+		}
+		sb.setCharAt(sb.length()-1, '}');
+
+		System.out.println(sb.toString());
+	}
+}
