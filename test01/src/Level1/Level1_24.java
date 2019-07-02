4b36fc01c599c1411f1b2ed43626521e7bdff846
Dextet0526 <soul9862@naver.com>
2019-07-02 11:05:15 +0900

    프로그래머스 Lv1 자릿수 더하기

---
+++ test01/src/Level1/Level1_24.java
@@ -0,0 +1,23 @@
+package Level1;
+
+// 자릿수 더하기
+public class Level1_24 {
+	public int solution(int n) {
+		int answer = 0;
+		int len = Integer.toString(n).length();
+		int cnt = 0;
+		while(cnt < len) {
+			answer += n % 10;
+			n /= 10;
+			cnt++;
+		}
+		return answer;
+	}
+
+
+	public static void main(String[] args) {
+		Level1_24 test = new Level1_24();
+		int n = 987;
+		System.out.println(test.solution(n));
+	}
+}
