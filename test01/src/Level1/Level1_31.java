fb5279a28cafdf38fbc4b06af5a1a51ffdc19e86
Dextet0526 <soul9862@naver.com>
2019-07-03 10:58:38 +0900

    프로그래머스 Lv1 문자열 내 p와 y의 개수

---
+++ test01/src/Level1/Level1_31.java
@@ -0,0 +1,29 @@
+package Level1;
+
+// 문자열 내 p와 y의 개수
+public class Level1_31 {
+
+	boolean solution(String s) {
+        boolean answer = true;
+        int countP = 0;
+        int countY = 0;
+        s = s.toLowerCase();
+        char[] temp = s.toCharArray();
+        for(int i = 0; i < temp.length; i++) {
+        	if(temp[i] == 'p') {
+        		countP++;
+        	}else if(temp[i] == 'y'){
+        		countY++;
+        	}
+        }
+        if(countP != countY) {
+        	answer = false;
+        }
+        return answer;
+    }
+	public static void main(String[] args) {
+		Level1_31 test = new Level1_31();
+		String s = "abd";
+		System.out.println(test.solution(s));
+	}
+}
