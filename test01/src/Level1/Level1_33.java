30df0a05143b91ea8ff3b0993020ec934f4675d3
Dextet0526 <soul9862@naver.com>
2019-07-03 14:29:47 +0900

    프로그래머스 Lv1 두 정수 사이의 합

---
+++ test01/src/Level1/Level1_33.java
@@ -0,0 +1,27 @@
+package Level1;
+
+// 두 정수 사이의 합
+public class Level1_33 {
+	public long solution(int a, int b) {
+		long answer = 0;
+		if(b > a) {
+			for(int i = a; i <= b; i++) {
+				answer += i;
+			}
+		}else if(a > b) {
+			for(int i = b; i <= a; i++) {
+				answer += i;
+			}
+		}else {
+			answer = a;
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Level1_33 test = new Level1_33();
+		int a = 0;
+		int b = 0;
+		test.solution(a, b);
+	}
+}
