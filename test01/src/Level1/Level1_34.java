35debb3bd28a14d1c6a107b195cfac052f0c2ced
Dextet0526 <soul9862@naver.com>
2019-07-04 12:05:46 +0900

    프로그래머스 Lv1 나누어 떨어지는 숫자 배열

---
+++ test01/src/Level1/Level1_34.java
@@ -0,0 +1,44 @@
+package Level1;
+
+import java.util.Arrays;
+
+// 나누어 떨어지는 숫자 배열
+public class Level1_34 {
+	public int[] solution(int[] arr, int divisor) {
+		int[] answer = {};
+		int[] temp = {};
+		int length = arr.length;
+		temp = new int[length];
+		int index = 0;
+		
+		for(int i = 0; i < length; i++) {
+			if(arr[i] % divisor == 0) {
+				temp[index] = arr[i];
+				index++;
+			}
+		}
+		if(index == 0) {
+			answer = new int[1];
+			answer[0] = -1;
+		}else {
+			answer = new int[index];
+			for(int i = 0; i < index; i++) {
+				answer[i] = temp[i];
+			}
+			Arrays.sort(answer);
+		}
+		
+		for(int i = 0; i < answer.length; i++) {
+			System.out.println(answer[i]);
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Level1_34 test = new Level1_34();
+		
+		int[] arr = {2, 36, 1, 3};
+		int divisor = 1;
+		test.solution(arr, divisor);
+	}
+}
