a7e1e414541cf9de3fb44a4835c133fcdf9c72e1
Dextet0526 <soul9862@naver.com>
2019-07-10 13:54:00 +0900

    프로그래머스 Lv2 점프와 순간 이동

---
+++ test01/src/Level2/Level2_09.java
@@ -0,0 +1,23 @@
+package Level2;
+
+// 점프와 순간 이동
+public class Level2_09 {
+//	5	6	5000	2	3	4	7	
+//	2	2	5		1	2	1	3
+	
+	 public int solution(int n) {
+	        int ans = 0;
+	        while(n >= 1) {
+	        	if(n % 2 == 0) {
+	        		n /= 2;
+	        	}else {
+	        		n /= 2;
+	        		ans++;
+	        	}
+	        }
+	        return ans;
+	    }
+	public static void main(String[] args) {
+		
+	}
+}
