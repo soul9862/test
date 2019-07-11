d785f885698028b7b53553bbbf9764f24fdcea38
Dextet0526 <soul9862@naver.com>
2019-07-11 09:37:40 +0900

    프로그래머스 Lv2 소수 만들기

--- test01/src/Level2/Level2_10.java
+++ test01/src/Level2/Level2_10.java
@@ -1,8 +1,6 @@
 package Level2;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 // 소수 만들기
 public class Level2_10 {
@@ -10,30 +8,35 @@ public class Level2_10 {
 	//	1			4
 	public int solution(int[] nums) {
 		int answer = 0;
+		int n = 0;
+		// 정렬
 		Arrays.sort(nums);
-		int n = nums[nums.length-1] 
-				+ nums[nums.length-2] 
-						+ nums[nums.length-3];
-		eratos(n);
+		// 3중문 3개 항목 추출
+		for(int i = 0; i < nums.length-2; i++) {
+			for(int j = i+1; j < nums.length-1; j++) {
+				for(int k = j+1; k < nums.length; k++) {
+					n = nums[i] + nums[j] + nums[k];
+					if(primeNumber(n) == true) answer++;
+				}
+			}
+		}
 
 		return answer;
 	}
-	public static List<Integer> eratos(int n) {
-		int[] num = new int[n+1];
-		int count = 0;
+	// 소수 판별
+	public static boolean primeNumber(int n) {
+		boolean answer = false;
 
-		for(int i = 2; i <= n; i++) {
-			num[i] = i;
-		}
-		for(int i = 2; (i*i) <= n; i++) {
-			if(num[i] == 0) continue;
-
-			for(int j = i*2; j <= n; j += i) {
-				num[j] = 0;
+		for(int i = 2; i < n; i++) {
+			if(n % i == 0) {
+				answer = false;
+				break;
+			}else {
+				answer = true;
 			}
 		}
-		List<Integer> intArr = Arrays.stream(num).boxed().collect(Collectors.toList());
-		return intArr;
+		
+		return answer;
 	}
 	public static void main(String[] args) {
 
