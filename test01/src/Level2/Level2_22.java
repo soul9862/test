4321351b48657c93cf6f7c7b4fa42e44dcc10fd8
Dextet0526 <soul9862@naver.com>
2019-07-16 10:42:30 +0900

    프로그래머스 Lv2 올바른 괄호

---
+++ test01/src/Level2/Level2_22.java
@@ -0,0 +1,24 @@
+package Level2;
+
+// ( = 40, ) = 41
+// ¿Ã¹Ù¸¥ °ýÈ£
+public class Level2_22 {
+	boolean solution(String s) {
+		boolean answer = true;
+		int x = 0;
+		int y = 0;
+		
+		if(s.charAt(0) == ')') answer = false;
+		else {
+			for(int i = 0; i < s.length(); i++) {
+				if(s.charAt(i) == '(') x++;
+				else if(s.charAt(i) == ')') y++;
+				
+				if(x < y) answer = false;
+			}
+			if(x != y) answer = false;
+		}
+		
+		return answer;
+	}
+}
