e3d0300f49cf7268b1e519b213e51c7702435c16
Dextet0526 <soul9862@naver.com>
2019-07-19 12:27:31 +0900

    프로그래머스 Lv2 라면공장

---
+++ test01/src/Level2/Level2_25.java
@@ -0,0 +1,26 @@
+package Level2;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+// 라면공장
+public class Level2_25 {
+	public int solution(int stock, int[] dates, int[] supplies, int k) {
+		int answer = 0;
+		Queue<Integer> Queue = new PriorityQueue<>(Comparator.reverseOrder());
+
+		int index = 0;
+		for (int i = 0; i < k; i++) {
+			if (index < dates.length && i == dates[index])
+				Queue.add(supplies[index++]);
+				
+			if (stock == 0) {
+				stock += Queue.poll();
+				answer++;
+			}
+			stock--;
+		}
+		return answer;
+	}
+}
