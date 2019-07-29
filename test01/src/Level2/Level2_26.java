7e2c8defea82e1deebb131de7d53fb91c6a1c726
DESKTOP-CFK662M\soul9 <soul9862@gmail.com>
2019-07-29 19:06:37 +0900

    프로그래머스 Lv2 타켓넘버(DFS, 티스토리 참조)

---
+++ test01/src/Level2/Level2_26.java
@@ -0,0 +1,17 @@
+package Level2;
+
+// 타켓넘버(DFS)
+public class Level2_26 {
+	public int solution(int[] numbers, int target) {
+        return DFS(numbers, target, 0, 0);
+    }
+ 
+    public int DFS(int[] numbers, int target, int index, int num) {
+        if(index == numbers.length) {
+            return num == target ? 1 : 0;
+        } else {	
+            return DFS(numbers, target, index + 1, num + numbers[index])
+                    + DFS(numbers, target, index + 1, num - numbers[index]);
+        }
+    }
+}
