1cf32d26412012481c11da02fdcb34d1814f0dc1
dexter <soul9862@gmail.com>
2021-06-28 21:35:17 +0900

    프로그래머스 1-43

--- Algorithm/src/programmers/level1/Level1_43.java
+++ Algorithm/src/programmers/level1/Level1_43.java
@@ -2,13 +2,30 @@ package Algorithm.src.programmers.level1;
 
 import java.util.Arrays;
 
+//두 개 뽑아서 더하기
 public class Level1_43 {
 
     public int[] solution(int[] numbers) {
         int[] answer = {};
 
+        int tempIdx = 0;
+
+        for(int i = numbers.length-1; i > 0; i--){
+            tempIdx += i;
+        }
+        int[] temp = new int[tempIdx];
+        System.out.println(tempIdx);
+
         Arrays.sort(numbers);
-        
+
+        for(int i = 0; i < numbers.length - 1; i++){
+            for(int j = i + 1; j < numbers.length; j++){
+                temp[tempIdx - 1] = numbers[i] + numbers[j];
+                System.out.println(tempIdx + " " + i + " " + j);
+                tempIdx--;
+            }
+        }
+        System.out.println(Arrays.toString(temp));
 
         return answer;
     }
--- Algorithm/src/programmers/level1/Level1_Testing.java
+++ Algorithm/src/programmers/level1/Level1_Testing.java
@@ -6,7 +6,7 @@ public class Level1_Testing {
 	public static void main(String[] args) {
 		
 		// testcase
-		int[] arr = {2,1,3,4,1};
+		int[] arr = {5,0,2,7};
 		
 		Level1_43 test = new Level1_43();
 
