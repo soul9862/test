8056d6ef8be3f31d87c8ab162c41b86ce2aa365e
dexter <soul9862@gmail.com>
2021-07-01 22:07:31 +0900

    ch03 퀵정렬

---
+++ Algorithm/src/hello/ch04/Quicksort.java
@@ -0,0 +1,37 @@
+package Algorithm.src.hello.ch04;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Quicksort {
+    public static void main(String[] args) {
+        System.out.println(quicksort(Arrays.asList(10, 5, 2, 3))); // [2, 3, 5, 10]
+    }
+
+    public static List<Integer> quicksort(List<Integer> array){
+
+        if(array.size() < 2) return array;
+        else{
+            Integer pivot = array.get(0);
+            List<Integer> less = array.stream()
+                    .skip(1)
+                    .filter(param -> param <= pivot)
+                    .collect(Collectors.toList());
+            List<Integer> greater = array
+                    .stream()
+                    .skip(1)
+                    .filter(param -> param > pivot)
+                    .collect(Collectors.toList());
+
+            return Stream.of(
+                    quicksort(less).stream(),
+                    Stream.of(pivot),
+                    quicksort(greater).stream())
+                    .flatMap(Function.identity()).collect(Collectors.toList());
+
+        }
+    }
+}
---
+++ Algorithm/src/hello/ch04/RecursiveSum.java
@@ -0,0 +1,16 @@
+package Algorithm.src.hello.ch04;
+
+import java.util.Arrays;
+
+public class RecursiveSum {
+    private static int sum(int[] arr) {
+
+        if (arr.length == 0) return 0;
+        else return arr[0] + sum(Arrays.copyOfRange(arr, 1, arr.length));
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(new int[]{5,4,3,6,8,31,23}));
+    }
+}
