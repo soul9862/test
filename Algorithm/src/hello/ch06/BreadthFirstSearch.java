813b93ee56202a2f8bb6a479569e13d6b719d1fe
dexter <dex9205@gmail.com>
2021-07-06 22:30:18 +0900

    ch06 너비 우선 탐색

---
+++ Algorithm/src/hello/ch06/BreadthFirstSearch.java
@@ -0,0 +1,44 @@
+package hello.ch06;
+
+import java.util.*;
+
+public class BreadthFirstSearch {
+    private static Map<String, List<String>> graph = new HashMap<>();
+
+    public static void main(String[] args) {
+        graph.put("you", Arrays.asList("alice", "bob", "claire"));
+        graph.put("bob", Arrays.asList("anuj", "peggy"));
+        graph.put("alice", Arrays.asList("peggy"));
+        graph.put("claire", Arrays.asList("thom", "jonny"));
+        graph.put("anuj", Collections.emptyList());
+        graph.put("peggy", Collections.emptyList());
+        graph.put("thom", Collections.emptyList());
+        graph.put("jonny", Collections.emptyList());
+
+        search("you");
+    }
+
+    public static boolean search(String name){
+        Queue<String> searchQueue = new ArrayDeque<>(graph.get(name));
+        List<String> searched = new ArrayList<>();
+
+        while (!searchQueue.isEmpty()){
+            String person = searchQueue.poll();
+            if(!searched.contains(person)){
+                if(personIsSeller(person)){
+                    System.out.println(person + " is a mango seller");
+                    return true;
+                }else{
+                    searchQueue.addAll(graph.get(person));
+                    searched.add(person);
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean personIsSeller(String name){
+        return name.endsWith("m");
+    }
+
+}
