bc9756628a9024ab441787545609841626873a12
dexter <dex9205@gmail.com>
2021-07-12 21:19:13 +0900

    ch07 다익스트라 알고리즘 연습문제 01

--- Algorithm/src/hello/ch07/DijkstrasAlgorithm.java
+++ Algorithm/src/hello/ch07/DijkstrasAlgorithm.java
@@ -12,31 +12,72 @@ public class DijkstrasAlgorithm {
 
 
     public static void main(String[] args) {
+        
+//        // graph table
+//        graph.put("start", new HashMap<>());
+//        graph.get("start").put("a", 6);
+//        graph.get("start").put("b", 2);
+//
+//        graph.put("a", new HashMap<>());
+//        graph.get("a").put("fin", 1);
+//
+//        graph.put("b", new HashMap<>());
+//        graph.get("b").put("a", 3);
+//        graph.get("b").put("fin", 5);
+//
+//        graph.put("fin", new HashMap<>());
+//
+//        // cost table
+//        Map<String, Integer> costs = new HashMap<>();
+//        costs.put("a", 6);
+//        costs.put("b", 2);
+//        costs.put("fin", Integer.MAX_VALUE);
+//
+//        // parents table
+//        Map<String, String> parents = new HashMap<>();
+//        parents.put("a", "start");
+//        parents.put("b", "start");
+//        parents.put("fin", null);
+//
+//        System.out.println(graph.get("start").get("a"));
 
+        // 연습 문제 01
         // graph table
         graph.put("start", new HashMap<>());
-        graph.get("start").put("a", 6);
+        graph.get("start").put("a", 5);
         graph.get("start").put("b", 2);
 
         graph.put("a", new HashMap<>());
-        graph.get("a").put("fin", 1);
+        graph.get("a").put("c", 4);
+        graph.get("a").put("d", 2);
 
         graph.put("b", new HashMap<>());
-        graph.get("b").put("a", 3);
-        graph.get("b").put("fin", 5);
+        graph.get("b").put("a", 8);
+        graph.get("b").put("d", 7);
+
+        graph.put("c", new HashMap<>());
+        graph.get("c").put("fin", 3);
+        graph.get("c").put("d", 6);
+
+        graph.put("d", new HashMap<>());
+        graph.get("d").put("fin", 1);
 
         graph.put("fin", new HashMap<>());
 
         // cost table
         Map<String, Integer> costs = new HashMap<>();
-        costs.put("a", 6);
+        costs.put("a", 5);
         costs.put("b", 2);
+        costs.put("c", Integer.MAX_VALUE);
+        costs.put("d", Integer.MAX_VALUE);
         costs.put("fin", Integer.MAX_VALUE);
 
         // parents table
         Map<String, String> parents = new HashMap<>();
         parents.put("a", "start");
         parents.put("b", "start");
+        parents.put("c", null);
+        parents.put("d", null);
         parents.put("fin", null);
 
         System.out.println(graph.get("start").get("a"));
